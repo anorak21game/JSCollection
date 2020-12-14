468437d6427a4fbeb530a1fb69b69f07440e0e83
Michael Wirth <mwirth@shiftphones.com>
2020-12-14 11:27:05 +0100

    not ready!

--- DesignPattern/Factory/index.js
+++ DesignPattern/Factory/index.js
@@ -1,43 +1,45 @@
-class AbstractFactory {
-  static create() {
-    if (this === AbstractFactory) throw new TypeError("Can not call static abstract method create.");
-    if (this.create === AbstractFactory.create) throw new TypeError("Please implement static abstract method create."); 
-    throw new TypeError("Do not call static abstract method create from child.");
-  }
-  constructor() {
-    if (this.constructor === AbstractFactory) throw new TypeError("Can not construct abstract class.");
-    if (this.create === AbstractFactory.prototype.create) throw new TypeError("Please implement abstract method create.");
-  }
-  create() {
-    throw new TypeError("Do not call abstract method create from child.");
-  }
-}
+// not ready yet !!!
 
-class ConcreteFactoryA extends AbstractFactory {
-  static create() {
-    // Implementation of abstract static method.
-  }
-  constructor() {
-    super();
-    // Implementation of constructor.
-  }
-  create() {
-    // Implementation of abstract method.
-  }
-}
+// class AbstractFactory {
+//   static create() {
+//     if (this === AbstractFactory) throw new TypeError("Can not call static abstract method create.");
+//     if (this.create === AbstractFactory.create) throw new TypeError("Please implement static abstract method create."); 
+//     throw new TypeError("Do not call static abstract method create from child.");
+//   }
+//   constructor() {
+//     if (this.constructor === AbstractFactory) throw new TypeError("Can not construct abstract class.");
+//     if (this.create === AbstractFactory.prototype.create) throw new TypeError("Please implement abstract method create.");
+//   }
+//   create() {
+//     throw new TypeError("Do not call abstract method create from child.");
+//   }
+// }
 
-class ConcreteFactoryB extends AbstractFactory {
-    static create() {
-      // Implementation of abstract static method.
-    }
-    constructor() {
-      super();
-      // Implementation of constructor.
-    }
-    create() {
-      // Implementation of abstract method.
-    }
-  }
+// class ConcreteFactoryA extends AbstractFactory {
+//   static create() {
+//     // Implementation of abstract static method.
+//   }
+//   constructor() {
+//     super();
+//     // Implementation of constructor.
+//   }
+//   create() {
+//     // Implementation of abstract method.
+//   }
+// }
+
+// class ConcreteFactoryB extends AbstractFactory {
+//     static create() {
+//       // Implementation of abstract static method.
+//     }
+//     constructor() {
+//       super();
+//       // Implementation of constructor.
+//     }
+//     create() {
+//       // Implementation of abstract method.
+//     }
+//   }
 
 // Terminal$>npm run test
 // quit npm Strg^C
