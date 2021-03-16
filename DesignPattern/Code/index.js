b21b9d7119794b8d6d4b3aba0339105338eefa59
freemium user <freemiumuser@example.com>
2021-03-16 16:47:26 +0100

    finish code challenge

--- DesignPattern/Code/index.js
+++ DesignPattern/Code/index.js
@@ -1,9 +1,43 @@
+// CODE CHALLENGE
+
+// incoming Number
+let newNumber = 0;
+
+// value how many are active
 const collapse = 3;
 
-let list = [1, 5, 5, 6, 9];
+// some list
+let list = [1, 2, 3];
+
+
+// delete all doubles
 const uniqueList = [...new Set(list)];
 list = uniqueList;
 
+// delete last elements more than collape value
+// while (list.length >= collapse) {
+//     list.pop();
+// }
+
+// move all elements one step next
+let item;
+let count;
+let listElement = collapse;
+let position = 0;
+
+count = 1;
+while (count <= collapse) {
+    listElement--;
+    item = list[listElement];
+    list[collapse - position] = item;
+    position++;
+    if (position == collapse) {
+        list[collapse - position] = newNumber;
+    }
+    count = count + 1;
+}
+
+// delete last elements more than collape value
 while (list.length > collapse) {
     list.pop();
 }
