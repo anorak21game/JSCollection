84e0361390be73cd1b8176dad2695926b0fc71fb
freemium user <freemiumuser@example.com>
2021-03-17 10:39:50 +0100

    hotfix code

--- DesignPattern/Code/index.js
+++ DesignPattern/Code/index.js
@@ -1,46 +1,22 @@
 // CODE CHALLENGE
 
-// incoming Number
-let newNumber = 0;
-
-// value how many are active
-const collapse = 3;
-
-// some list
-let list = [1, 2, 3];
+let newPanel = 9;
+const openPanels = 3;
+let panels = [1, 1, 7, 8, 1, 7, 3, 9, 2, 15]; 
 
 
-// delete all doubles
-const uniqueList = [...new Set(list)];
-list = uniqueList;
+// delete duplicate panels
+const uniquePanels = [...new Set(panels)];
+panels = uniquePanels;
 
-// delete last elements more than collape value
-// while (list.length >= collapse) {
-//     list.pop();
-// }
+// add new panel
+panels.unshift(newPanel);
 
-// move all elements one step next
-let item;
-let count;
-let listElement = collapse;
-let position = 0;
-
-count = 1;
-while (count <= collapse) {
-    listElement--;
-    item = list[listElement];
-    list[collapse - position] = item;
-    position++;
-    if (position == collapse) {
-        list[collapse - position] = newNumber;
-    }
-    count = count + 1;
+// delete too many panels
+while (panels.length > openPanels) {
+    panels.pop();
+    // Action.do close !!!
 }
 
-// delete last elements more than collape value
-while (list.length > collapse) {
-    list.pop();
-}
-
-console.log(list);
+console.log(panels);
 
