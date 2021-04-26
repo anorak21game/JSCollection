c6d1372122efeb3f97fb8aebec23f30778f5dba0
Michael Wirth <mwirth@shiftphones.com>
2021-04-26 15:12:42 +0200

    add new code

--- AhaOfTheWeek/Objekte/DOM/main.js
+++ AhaOfTheWeek/Objekte/DOM/main.js
@@ -142,11 +142,101 @@ console.log(last_elemnet_child);
 
 // sibling element
 
-let next_element_sibling = ul.nextElementSibling;
-let previous_element_sibling = ul.previousElementSibling;
+let next_element_sibling = ul.firstElementChild.nextElementSibling;
+let previous_element_sibling = ul.lastElementChild.previousElementSibling;
 
 console.log(next_element_sibling);
 console.log(previous_element_sibling);
+
+// parent element
+
+let parent_element = ul.parentElement;
+
+console.log(parent_element);
+
+// navigation
+
+let anchor = ul.querySelectorAll("li > a");
+
+console.log(anchor);
+
+console.log("\n---------------------------------------------------------------");
+console.log('%cDOM: %cappend child', 'color: blue', 'color: green');
+console.log("\n");
+
+let div = document.createElement("div");
+div.setAttribute("id", "my_id");
+div.setAttribute("class", "my_class");
+
+// append child
+
+let text = document.createTextNode("APPEND CHILD TEST");
+div.appendChild(text);
+
+let jumbo_section = document.querySelector(".jumbotron > section");
+jumbo_section.appendChild(div);
+
+let nav_li = document.querySelector("#navigation > ul > li");
+console.log(nav_li);
+
+let nav_ul = document.querySelector("#navigation > ul");
+console.log(nav_ul);
+
+nav_ul.appendChild(nav_li);
+
+console.log("\n---------------------------------------------------------------");
+console.log('%cDOM: %cinnerHTML', 'color: blue', 'color: green');
+console.log("\n");
+
+let jumbo_html = document.querySelector(".jumbotron");
+
+console.log(jumbo_html.innerHTML); // HTML
+console.log(jumbo_html.innerText); // render Text
+console.log(jumbo_html.textContent); // string Text
+
+let string_1 = "";
+let string_2 = "<header><h2>HELLO</h2></header>";
+let string_3 = "<p>WORLD</p>";
+let string_4 = "Lorem ipsum doloar sit amet.";
+
+jumbo_html.innerHTML = string_2;
+
+console.log("\n---------------------------------------------------------------");
+console.log('%cDOM: %cinsertAdjacent', 'color: blue', 'color: green');
+console.log("\n");
+
+let li_el = document.createElement("li");
+li_el.setAttribute("id", "mein-listenelement");
+
+let a_el = document.createElement("a");
+a_el.setAttribute("id", "mein-ankerelement");
+a_el.setAttribute("href", "#");
+
+let text_node = document.createTextNode("Element");
+
+a_el.appendChild(text_node);
+li_el.appendChild(a_el);
+
+let liste = document.querySelector("#navigation > ul");
+
+// liste.insertAdjacentElement("beforebegin", li);
+// liste.insertAdjacentElement("afterbegin", li);
+// liste.insertAdjacentElement("beforeend", li);
+// liste.insertAdjacentElement("afterend", li);
+
+let dom_string = "<li id=\"mein-listenelement\"><a id=\"mein-ankerelement\" href=\"#\">Element</a></li>";
+
+// liste.insertAdjacentHTML("beforebegin", dom_string);
+// liste.insertAdjacentHTML("afterbegin", dom_string);
+// liste.insertAdjacentHTML("beforeend", dom_string);
+// liste.insertAdjacentHTML("afterend", dom_string);
+
+let text_list = "Lorem ipsum dolor sit amet.";
+
+// liste.insertAdjacentText("beforebegin", text);
+// liste.insertAdjacentText("afterbegin", text);
+// liste.insertAdjacentText("beforeend", text);
+liste.insertAdjacentText("afterend", text_list);
 
 
 
