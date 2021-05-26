15cdd6c827498ca0efb80ed6a73b8c65bc3870f6
Michael Wirth <mwirth@shiftphones.com>
2021-05-26 10:22:06 +0200

    add sample code login function

---
+++ CopyPast/Keycloak/Login Function/sample.js
@@ -0,0 +1,14 @@
+// ShifterNetwork.class
+// 468
+
+// session.login('messages');
+
+// KeycloakSessionManager.class
+// 155
+
+// login(page) {
+//     if (page == undefined) {
+//         page = "login";
+//     }
+//     const url = this._private.keycloak.createLoginUrl({ 'redirectUri': this.IOR.loader.baseUrl + this.IOR.loader.basePath + '/src/html/KeycloakResponsePageLogin.html' + '?callFrom=' + page });
+// }
